1381cb9107d4df723bbce9fa5c99dfa22e302d93
chandana-2006-04 <chandanayadav297@gmail.com>
2026-01-31 16:18:01 +0530

    Add files via upload

---
+++ text.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class text
+{
+    public static void main(String args[])
+    {
+    Scanner obj=new Scanner(System.in);
+    System.out.println("marks");
+    int marks=obj.nextInt();
+    System.out.println("enter the marks");
+    double a=obj.nextDouble();
+    System.out.println("enter the a value");
+    float b=obj.nextFloat(); 
+    System.out.println("enter the b value");
+    String Str1=obj.next();
+    System.out.println("enter Str1 value");
+    String Str2=obj.nextLine();
+    System.out.println("enter the Str2 value");
+    }
+ }
