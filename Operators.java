23e5d18c7abb34f3e49e86de238d17acecd2cfab
chandana-2006-04 <chandanayadav297@gmail.com>
2026-02-07 14:55:03 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,15 @@
+public class Operators{
+     public static void main(String[] args){
+        int a=56;
+        int b=46;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a&b);
+        System.out.println(a*b);
+        System.out.println(a^b);
+    }
+}
