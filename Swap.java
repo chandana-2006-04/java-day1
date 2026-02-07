46f28b223831667f4e3376d328e9017a29369022
chandana-2006-04 <chandanayadav297@gmail.com>
2026-02-07 15:19:34 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,11 @@
+ public class Swap{
+    public static void main(String[] args) {
+        int a=13;
+        int b=67;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+ }
