a5e016150d3b5288bcef3c8d48fc45c494040750
chandana-2006-04 <chandanayadav297@gmail.com>
2026-02-07 14:55:58 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,11 @@
+public class Arithmetic{
+    public static void main(String args[]){
+    int a=56;
+    int b=76;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+    }
+}
